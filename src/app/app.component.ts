85abd2243b40ac4958cb98cca76e8c9ef6f9543d
Fanny Dacheux <fanny.dacheux@gmail.com>
2023-02-16 17:57:46 +0100

    feat(detail pokemon component): add detail pokemon component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,11 @@
-import { Component, OnInit } from "@angular/core";
-import { POKEMONS } from "./mock-pokemon-list";
-import Pokemon from "./pokemon";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: 'app.component.html', //vue
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
    //classe: tout ce qui va permettre de faire fonctionner la vue correctement
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon | undefined;
 
-  ngOnInit(): void {
-    console.table(this.pokemonList);
-  }
-
-  selectPokemon(pokemonId: string): void {
-    const pokemon: Pokemon | undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
-    if (pokemon) {
-      console.log(`${pokemon.name}, I chose you ! `);
-      this.pokemonSelected = pokemon;
-    } else {
-      console.log("There is no such pokemon... yet");
-      this.pokemonSelected = pokemon;
-    }
-  }
 }
--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { POKEMONS } from '../mock-pokemon-list';
+import Pokemon from '../pokemon';
 
 @Component({
   selector: 'app-detail-pokemon',
@@ -6,6 +9,18 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class DetailPokemonComponent {
+export class DetailPokemonComponent implements OnInit {
 
+  pokemonList: Pokemon[] | undefined;
+  pokemon: Pokemon | undefined;
+
+  constructor(private router: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.pokemonList = POKEMONS
+    const pokemonId: string | null = this.router.snapshot.paramMap.get('id');
+    if (pokemonId) {
+      this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+    } 
+  }
 }
